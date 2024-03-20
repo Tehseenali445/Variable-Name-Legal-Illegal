f8c40f933e4146ea7f4896130b510419d6059dc1
Tehseen Ali <154703743+Tehseenali445@users.noreply.github.com>
2024-03-20 21:36:36 +0500

    Variable Name: Legal & Illegal
    Variable Name: Legal & Illegal

---
+++ app.js
@@ -0,0 +1,62 @@
+// Q1: Declare 3 variables in one statement.
+
+// let name_1 = 44, name_2 = 54, name_3 = 64;
+
+// console.log(name_1, name_2, name_3);
+
+// Q2: Declare 5 legal & 5 illegal variable names.
+
+// Legal Variable Names:
+
+// 1.
+// let myNametehseen = 2;
+// 2.
+// let _myNametehseen = 3;
+// 3.
+// let $myNametehseen = 4;
+// 4.
+// let myNameTehseen = 5;
+// 5.
+// let myNametehseen123 = 6;
+
+// // Illegal Variable Names:
+
+// 1.
+// let 123myNametehseen = 2;
+// 2.
+// let my Nametehseen = 3;
+// 3.
+// let !myNametehseen = 4;
+// 4.
+// let var = 5;
+// 5.
+// let my-Nametehseen = 5;
+
+// Q3: Display this in your browser.
+
+// a)
+
+let heading = "<h2>Rules for naming JS variables</h2>";
+document.write(heading, "<br/>" + "<br/>");
+
+// b)
+
+document.write(
+  "Variable names can only contain , numbers, $ and _. For example: $my_1stVariable" +
+    "<br/>"
+);
+
+// c)
+
+document.write(
+  "Variables must begin with a letter, $ or _. For Example: $name, _name or name" +
+    "<br/>"
+);
+
+//  d)
+
+document.write("Variable names are case sensitive" + "<br/>");
+
+// e)
+
+document.write("Variable names should not be JS keyword" + "<br/>");
